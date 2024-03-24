456d40cf702980da2fc0dfc97494262e9dd308c1
Tomer Shalev <tomer.shalev@gmail.com>
2024-03-24 21:39:07 +0200

    minor refactor

--- packages/database-mongodb-atlas-data-api/tests/runner.test.js
+++ packages/database-mongodb-atlas-data-api/tests/runner.test.js
@@ -2,15 +2,13 @@ import { App } from '@storecraft/core';
 import { MongoDB } from '@storecraft/database-mongodb-atlas-data-api';
 import { NodePlatform } from '@storecraft/platform-node';
 import  { api_index } from '@storecraft/test-runner'
-export const admin_email = 'admin@sc.com';
-export const admin_password = 'password';
 
 export const create_app = async () => {
   let app = new App(
     new NodePlatform(),
     new MongoDB(),
     null, null, null, {
-      admins_emails: [admin_email],
+      admins_emails: ['admin@sc.com'],
       auth_password_hash_rounds: 100,
       auth_secret_access_token: 'auth_secret_access_token',
       auth_secret_refresh_token: 'auth_secret_refresh_token'

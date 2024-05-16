c15cf8d9d63d6c07ad9250cffff4d4655c47afd0
Tomer Shalev <tomer.shalev@gmail.com>
2024-05-16 13:33:46 +0300

    test-runner: fixed tags test

--- packages/test-runner/api_tags_crud_test.js
+++ packages/test-runner/api_tags_crud_test.js
@@ -50,28 +50,7 @@ export const create = app => {
     }
   );
 
-  // return s;
   add_sanity_crud_to_test_suite(s);
-
-
-  s('upsert tag with bad handle', async () => {
-    /** @type {import('@storecraft/core/v-api').TagTypeUpsert} */
-    const tag_insert = {
-      handle: 'tag 2', values:['a', 'b']
-    }
-    await assert_async_throws(
-      async () => await app.api.tags.upsert({
-        handle: 'tag 2', values: ['a', 'b']
-      })
-    );
-
-    await assert_async_throws(
-      async () => await app.api.tags.upsert({
-        handle: 'tag-2', values: ['a c', 'b']
-      })
-    );
-
-  })
 
   return s;
 }

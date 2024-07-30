0e70f3f75791fa4bb67309df886b8eef93879501
Tomer Shalev <tomer.shalev@gmail.com>
2024-07-30 13:34:16 +0300

    core, rest: first demo for export collections, next is testing and dashboard

--- packages/core/v-api/con.collections.logic.js
+++ packages/core/v-api/con.collections.logic.js
@@ -1,4 +1,4 @@
-import { to_handle } from './utils.func.js'
+import { assert, to_handle } from './utils.func.js'
 import { collectionTypeUpsertSchema } from './types.autogen.zod.api.js'
 import { regular_get, regular_list, 
   regular_remove, regular_upsert } from './con.shared.js'
@@ -47,12 +47,62 @@ export const list_collection_products = (app) =>
   /**
    * 
    * @param {import('../v-database/types.public.js').HandleOrId} handle_or_id 
-   * @param {import('./types.api.query.js').ApiQuery} q 
+   * @param {import('./types.api.query.js').ApiQuery} [q] 
    */
   (handle_or_id, q) => {
     return db(app).list_collection_products(handle_or_id, q);
   }
 
+
+/**
+ * @description Export a colletion of `products` into the `storage`. This is
+ * beneficial for `collections`, that hardly change and therefore can be 
+ * efficiently stored in a cost-effective `storage` and **CDN** network.
+ * 
+ * 
+ * @param {import("../types.public.js").App} app
+ */
+export const export_collection = (app) => 
+  /**
+   * 
+   * @param {import('../v-database/types.public.js').HandleOrId} handle_or_id 
+   * 
+   * @return {Promise<string>}
+   */
+  async (handle_or_id) => {
+    const collection = await inter(app).get(handle_or_id);
+
+    assert(
+      collection,
+      'export failed'
+    );
+
+    const items = await list_collection_products(app)(handle_or_id);
+    const encoder = new TextEncoder();
+    const array = encoder.encode(JSON.stringify(items));
+
+    const key = `collections/${collection.handle}`;
+    const publish_path = `storage://${key}`;
+    const success = await app.storage.putArraybuffer(
+      key,
+      array
+    );
+
+    assert(
+      success,
+      'export failed'
+    );
+
+    await upsert(app)(
+      {
+        ...collection,
+        published: publish_path
+      }
+    );
+
+    return publish_path;
+  }
+  
 
 /**
  * 
@@ -65,7 +115,8 @@ export const inter = app => {
     upsert: upsert(app),
     remove: regular_remove(app, db(app), 'collections/remove'),
     list: regular_list(app, db(app), 'collections/list'),
-    list_collection_products: list_collection_products(app)
+    list_collection_products: list_collection_products(app),
+    export_collection: export_collection(app)
   }
 }
 

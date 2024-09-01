01551d9e639d83d20341478079fe2f1153b6eee0
Tomer Shalev <tomer.shalev@gmail.com>
2024-09-01 22:01:58 +0300

    platform, cli: improvemtns

--- packages/playground/deno/index.ts
+++ packages/playground/deno/index.ts
@@ -2,7 +2,7 @@ import { app } from './app.ts';
 
 await app.init(false);
 
-const server = Deno.serve(
+Deno.serve(
   {
     onListen(d) {
       app.print_banner(`http://${d.hostname}:${d.port}`);

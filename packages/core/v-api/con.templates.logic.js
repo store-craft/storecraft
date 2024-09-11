39474ef9c1878dff9202ab5780d438b770fc0216
Tomer Shalev <tomer.shalev@gmail.com>
2024-09-11 13:43:57 +0300

    sql: added templates upsert support with base64 to overcome the faultyness of d1 driver

--- packages/core/v-api/con.templates.logic.js
+++ packages/core/v-api/con.templates.logic.js
@@ -2,6 +2,7 @@ import { assert, to_handle } from './utils.func.js'
 import { templateTypeUpsertSchema } from './types.autogen.zod.api.js'
 import { regular_get, regular_list, 
   regular_remove, regular_upsert } from './con.shared.js'
+import { decode } from '../v-crypto/base64.js';
 
 /**
  * @typedef {import('./types.api.d.ts').TemplateType} ItemType
@@ -12,6 +13,19 @@ import { regular_get, regular_list,
  * @param {import("../types.public.d.ts").App} app
  */
 export const db = app => app.db.resources.templates;
+
+/**
+ * @description if `base64_` prefixed then decode the postfix and return it,
+ * otherwise, just return the original value.
+ * @param {string} val 
+ */
+const decode_if_base64 = val => {
+  if(!val.startsWith('base64_'))
+    return val;
+
+  const b64 = val.split('base64_').at(1) ?? '';
+  return decode(b64);
+}
 
 /**
  * 
@@ -25,9 +39,12 @@ export const upsert = (app) =>
 (item) => regular_upsert(
   app, db(app), 'template', templateTypeUpsertSchema, 
   (before) => {
+    console.log(before)
     return {
       ...before,
-      handle: before.handle ?? to_handle(before.title)
+      handle: before.handle ?? to_handle(before.title),
+      template_html: decode_if_base64(before.template_html),
+      template_text: decode_if_base64(before.template_text),
     }
   },
   (final) => {

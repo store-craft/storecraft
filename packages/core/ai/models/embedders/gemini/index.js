d7a1768456548af87389e572a42ed98283c32066
Tomer Shalev <tomer.shalev@gmail.com>
2025-03-02 14:20:56 +0200

    ai: added gemini embedder

---
+++ packages/core/ai/models/embedders/gemini/index.js
@@ -0,0 +1,87 @@
+/**
+ * @import { 
+ *  config, RequestBody, RequestResult
+ * } from "./types.js";
+ * @import { 
+ *  GenerateEmbeddingsParams, GenerateEmbeddingsResult, AIEmbedder
+ * } from "../../../core/types.private.js";
+ */
+
+
+
+/**
+ * @typedef {AIEmbedder<config>} Impl
+ */
+
+const strip_leading = (text = '') => {
+  return (text[0]==='/') ? text.slice(1) : text;
+}
+
+// https://generativelanguage.googleapis.com/v1beta/models/text-embedding-004:embedContent?key=$GOOGLE_API_KEY
+
+/**
+ * @implements {Impl}
+ */
+export class GeminiEmbedder {
+  #embeddings_url = '';
+
+  /**
+   * @param {config} config 
+   */
+  constructor(config) {
+    this.config = {
+      ...config,
+      model: config.model ?? 'text-embedding-004',
+      api_version: config.api_version ?? 'v1beta'
+    }
+
+    this.#embeddings_url = new URL(
+      strip_leading(this.config.api_version + '/models/' + this.config.model + ':batchEmbedContents?key=' + this.config.api_key), 
+      'https://generativelanguage.googleapis.com'
+    ).toString();
+
+  }
+
+  /** @type {Impl["generateEmbeddings"]} */
+  generateEmbeddings = async (params) => {
+
+    const body = (/** @type {RequestBody} */ (
+      {
+        requests: params.content.filter(
+          c => c.type==='text'
+        ).map(
+          c => (
+            {
+              model: this.config.model,
+              content: {
+                parts: params.content.map(c => ({text: c.content}))
+              }
+            }
+          )
+        )
+      }
+    ));
+
+    const r = await fetch(
+      this.#embeddings_url,
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      }
+    );
+
+    /** @type {RequestResult} */
+    const json = await r.json();
+
+    return {
+      content: json.embeddings.map(
+        o => o.values
+      )
+    }
+  }
+
+}
+

9cb3f52a917e6826f6c976161b2686744eaae6a5
Tomer Shalev <tomer.shalev@gmail.com>
2025-03-05 22:21:25 +0200

    open-api: removed the yaml but keeping the json to reduce size of npm package

--- packages/core/ai/models/vector-stores/pinecone/types.d.ts
+++ packages/core/ai/models/vector-stores/pinecone/types.d.ts
@@ -1,11 +1,10 @@
-import type { AIEmbedder } from '../../../types.public.js';
+import { type AIEmbedder } from '../../../types.public.js';
 export * from './index.js';
 
 export type Config = {
   /** If absent, will be infered from environment variable `PINECONE_API_KEY` */
   api_key?: string;
   
-  /** This is a `url` where you should query the index, get it from the console | describe api call */
   /**
    * @description The index name
    * @default 'vector-index'

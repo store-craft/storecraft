90a398d12bf02784ad96d38f83550830de77d1d8
Tomer Shalev <tomer.shalev@gmail.com>
2025-03-06 23:32:45 +0200

    core: full query utils test suite

--- packages/core/api/utils.query.test.js
+++ packages/core/api/utils.query.test.js
@@ -1,8 +1,17 @@
+/** 
+ * @import { ApiQuery, Tuple } from '../api/types.api.query.js';
+ */
 import 'dotenv/config';
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import { file_name } from '../test-runner/api/api.utils.crud.js';
-import { EXPAND, is_string_a_number, parse_expand, parse_list_from_string, parse_number_from_string, parse_sortby, parse_tuples, parse_tuples_old, parse_value_part } from './utils.query.js';
+import { 
+  api_query_to_searchparams, EXPAND, is_string_a_number, parse_expand, 
+  parse_list_from_string, parse_number_from_string, parse_query, 
+  parse_sortby, parse_tuples, parse_tuples_old, parse_value_part 
+} from './utils.query.js';
+import { assert_partial } from '../test-runner/api/utils.js';
+import { parse } from '../vql/index.js';
 
 
 const s = suite(
@@ -109,7 +118,7 @@ s('parse_expand', async () => {
   }
 });
 
-s('parse_list_from_string', async () => {
+s('parse_sortby', async () => {
   const cases = [
     {
       value: 'a,b,c',
@@ -159,6 +168,171 @@ s('parse_number_from_string', async () => {
   }
 });
 
+s('api_query_to_url_query_params_and_back', async () => {
+  /** @type {ApiQuery[]} */
+  const cases = [
+    {
+      endAt: [['updated_at', '2012'], ['price', 100], ['active', true]],
+      expand: ['products'],
+      limit: 10,
+      order: 'asc',
+      sortBy: ['updated_at', 'price', 'active'],
+      startAt: [['updated_at', '"2012"'], ['price', 50], ['active', true]],
+      vql: '-a | (b & c)'
+    },
+    {
+      endBefore: [['updated_at', '"2012"'], ['price', 100], ['active', true]],
+      sortBy: ['updated_at', 'price', 'active'],
+      expand: ['products'],
+      limitToLast: 10,
+      order: 'desc',
+      startAfter: [['updated_at', '"2012"'], ['price', 50], ['active', true]],
+      vql: '-a | (b & c)'
+    },
+  ];
+
+  for(const c of cases) {
+    const q_params = api_query_to_searchparams(c);
+    const api_query = parse_query(q_params);
+
+    assert_partial(
+      api_query,
+      c
+    );
+  }
+});
+
+s('parse_query_also_parses_VQL', async () => {
+  /** @type {ApiQuery[]} */
+  const cases = [
+    {
+      vql: '-a | (b & c)'
+    },
+  ];
+
+  for(const c of cases) {
+    const q_params = api_query_to_searchparams(c);
+    const api_query = parse_query(q_params);
+    assert.snapshot(
+      JSON.stringify(api_query.vqlParsed),
+      JSON.stringify(parse(c.vql))
+    );
+  }
+});
+
+s('api_query_equals_maps_to_start_and_end', async () => {
+  /** @type {ApiQuery[]} */
+  const cases = [
+    {
+      equals: [['updated_at', '2012'], ['id', 'id_1']]
+    },
+  ];
+
+  for(const c of cases) {
+    const q_params = api_query_to_searchparams(c);
+    const api_query = parse_query(q_params);
+    assert.snapshot(
+      JSON.stringify(api_query.startAt),
+      JSON.stringify(api_query.endAt)
+    );
+    assert.snapshot(
+      JSON.stringify(api_query.startAt),
+      JSON.stringify(c.equals)
+    );
+  }
+});
+
+
+s('range_query_overrides_sort_cursor', async () => {
+  /** @type {Tuple<string>[]} */
+  const range_cursor = [['updated_at', '2012'], ['id', 'id_1']];
+
+  /** @type {ApiQuery[]} */
+  const cases = [
+    {
+      equals: range_cursor,
+      sortBy: ['fake']
+    },
+    {
+      startAt: range_cursor,
+      sortBy: ['fake']
+    },
+    {
+      startAfter: range_cursor,
+      sortBy: ['fake']
+    },
+    {
+      endAt: range_cursor,
+      sortBy: ['fake']
+    },
+    {
+      endBefore: range_cursor,
+      sortBy: ['fake']
+    },
+  ];
+
+  for(const c of cases) {
+    const q_params = api_query_to_searchparams(c);
+    const api_query = parse_query(q_params);
+    assert.snapshot(
+      JSON.stringify(api_query.sortBy),
+      JSON.stringify(range_cursor.map(it => it[0]))
+    );
+  }
+});
+
+s('non_matching_range_cursor_keys_throw', async () => {
+  /** @type {Tuple<string>[]} */
+  const range_cursor = [['updated_at', '2012'], ['id', 'id_1']];
+
+  /** @type {ApiQuery[]} */
+  const cases = [
+    {
+      startAt: [['updated_at', '2012'], ['id', 'id_1']],
+      endAt: [['updated_at', '2012'], ['x', 'id_1']]
+    },
+    {
+      startAfter: [['updated_at', '2012'], ['id', 'id_1']],
+      endBefore: [['updated_at', '2012'], ['x', 'id_1']]
+    },
+  ];
+  
+  for(const c of cases) {
+    assert.throws(
+      () => {
+        const q_params = api_query_to_searchparams(c);
+        const api_query = parse_query(q_params);
+      }
+    );
+  }
+});
+
+
+s('setting_same_direction_range_cursor_throws', async () => {
+  /** @type {Tuple<string>[]} */
+  const range_cursor = [['updated_at', '2012'], ['id', 'id_1']];
+
+  /** @type {ApiQuery[]} */
+  const cases = [
+    {
+      startAt: [['updated_at', '2012'], ['id', 'id_1']],
+      startAfter: [['updated_at', '2012'], ['x', 'id_1']]
+    },
+    {
+      endAt: [['updated_at', '2012'], ['id', 'id_1']],
+      endBefore: [['updated_at', '2012'], ['x', 'id_1']]
+    },
+  ];
+
+  for(const c of cases) {
+    assert.throws(
+      () => {
+        const q_params = api_query_to_searchparams(c);
+        const api_query = parse_query(q_params);
+      }
+    );
+  }
+});
 
 
 s.run();
